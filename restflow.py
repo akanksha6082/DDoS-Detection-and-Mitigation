64e920425d12b1729e15593cb4de4ca89cc8b9ff
Akanksha <shahakanksha286@gmail.com>
2022-01-31 12:57:47 +0530

    updated sflow-rt URL

--- restflow.py
+++ restflow.py
@@ -3,8 +3,8 @@ import requests
 import json
 import signal
 
-rt = 'http://10.0.0.162:8008'
-name = 'tcp'
+rt = 'http://127.0.0.1:8008'
+name = 'icmp'
 
 def sig_handler(signal,frame):
   requests.delete(rt + '/flow/' + name + '/json');
@@ -14,17 +14,21 @@ signal.signal(signal.SIGINT, sig_handler)
 flow = {'keys':'ipsource,ipdestination,macsource,macdestination',
         'value':'frames',
         'log':True}
+        
 r = requests.put(rt + '/flow/' + name + '/json',data=json.dumps(flow))
 
 flowurl = rt + '/flows/json?name=' + name + '&maxFlows=10&timeout=20'
 flowID = -1
+
 while 1 == 1:
   r = requests.get(flowurl + "&flowID=" + str(flowID))
   if r.status_code != 200: break
   flows = r.json()
   if len(flows) == 0: continue
+  else:
+    print(len(flows))
 
   flowID = flows[0]["flowID"]
   flows.reverse()
   for f in flows:
-    print str(f['flowKeys']) + ',' + str(int(f['value'])) + ',' + str(f['end'] - f['start']) + ',' + f['agent'] + ',' + str(f['dataSource'])
+    print(str(f['flowID']) + "," + str(f['flowKeys']) + ',' + str(int(f['value'])) + ',' + str(f['end'] - f['start']) + ',' + f['agent'] + ',' + str(f['dataSource']))
