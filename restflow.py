c70cd3dc5b92294b0a54210bcd403d7b2069c417
Akanksha <shahakanksha286@gmail.com>
2022-02-01 22:28:35 +0530

    redirected flows to file

--- restflow.py
+++ restflow.py
@@ -22,11 +22,33 @@ flowurl = rt + '/flows/json?name=' + name + '&maxFlows=10&timeout=20'
 flowID = -1
 
 while 1 == 1:
-  r = requests.get(flowurl + "&flowID=" + str(flowID))
-  if r.status_code != 200: break
-  flows = r.json()
-  if len(flows) == 0: continue
-  flowID = flows[0]["flowID"]
-  flows.reverse()
-  for f in flows:
-    print(str(f['flowID']) + "," + str(f['flowKeys']) + ',' + str(int(f['value'])) + ',' + str(f['end'] - f['start']) + ',' + f['agent'] + ',' + str(f['dataSource']))
+    
+    r = requests.get(flowurl + "&flowID=" + str(flowID))
+    
+    if r.status_code != 200: 
+        break
+    
+    flows = r.json()
+    
+    if len(flows) == 0: 
+        continue
+    
+    flowID = flows[0]["flowID"]
+    flows.reverse()
+
+    for f in flows:
+        
+        flow_id = str(f['flowID'])
+        src_ip, dest_ip, src_mac, dest_mac = f['flowKeys'].split(',')
+        datasource = str(f['dataSource'])
+        flow_name = f['name']
+        
+        delimiter = "_SFLOW_"
+
+        flow = "flow_id=" + flow_id + delimiter + "src_ip=" + src_ip + delimiter + "dest_ip=" + dest_ip + delimiter + "src_mac=" + src_mac + delimiter + "dest_mac=" + dest_mac + delimiter + "flow_name=" + flow_name + "\n\n"
+
+        with open("data.txt", "a") as data_file:
+            data_file.write(flow)
+
+        print(flow)
+
